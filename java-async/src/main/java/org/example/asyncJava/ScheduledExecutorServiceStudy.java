639cf9feb971403b46021210d1e8d0c795d0fa2b
benggrae <kbh0581@gmail.com>
2023-09-20 02:15:24 +0900

    ADD: 스케쥴링 추가

--- java-async/src/main/java/org/example/asyncJava/ScheduledExecutorServiceStudy.java
+++ java-async/src/main/java/org/example/asyncJava/ScheduledExecutorServiceStudy.java
@@ -1,5 +1,6 @@
 package org.example.asyncJava;
 
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -12,19 +13,21 @@ public class ScheduledExecutorServiceStudy {
     public static void main(String[] args) throws InterruptedException {
         ScheduledExecutorService es = Executors.newScheduledThreadPool(2);
 
-
         // 1초 후에 1번 작업 실행 하고 1분뒤에 다음 작업을 수행한다.
-        es.schedule(() -> log.info("1번 작업"), 1, TimeUnit.MINUTES);
-        es.schedule(() -> log.info("2번 작업"), 1, TimeUnit.MINUTES);
-        es.schedule(() -> log.info("3번 작업"), 1, TimeUnit.SECONDS);
-        es.schedule(() -> log.info("4번 작업"), 1, TimeUnit.SECONDS);
+        ScheduledExecutorService roop = Executors.newScheduledThreadPool(4);
+        roop.scheduleAtFixedRate(() -> {
+                log.info("scheduleAtFixedRate 반복 작업");
+                    try {Thread.sleep(1000);}  catch (InterruptedException e) {}
+                }, 0, 1, TimeUnit.SECONDS);
 
-
-        ScheduledExecutorService roop = Executors.newScheduledThreadPool(100);
-        roop.scheduleAtFixedRate(() -> log.info("반복 작업"), 0, 1, TimeUnit.SECONDS);
+        roop.scheduleWithFixedDelay(() -> {
+                    log.info("scheduleWithFixedDelay 반복 작업");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {}
+                }, 0, 1, TimeUnit.SECONDS);
 
         es.shutdown();
-        //roop.shutdown();
     }
 
 }
