65a3e16751051d2ffd8297d4f75f3fc829a2e423
beng9re <kbh0581@gmail.com>
2023-10-10 01:23:03 +0900

    fix: 비동기 스프링 이벤트
    파일누락수정

--- src/main/java/com/example/springevent/step2_asyncEvent/AsyncSpringEventsConfig.java
+++ src/main/java/com/example/springevent/step2_asyncEvent/AsyncSpringEventsConfig.java
@@ -1,10 +1,6 @@
 package com.example.springevent.step2_asyncEvent;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
 public class AsyncSpringEventsConfig {
